2d88cce481ecfe8bacccdc5f362df6e6e380cdd8
sukaran <sukarankant765@gmail.com>
2022-08-05 14:26:05 +0530

    add campground model and dummy data

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+// Connectb with mongoDB 
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+.then(()=>{
+    console.log("database open");
+})
+.catch(err=>{
+    console.log(err);
+})
 
 const app = express()
 
@@ -7,8 +18,12 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 
-app.get('/', (req, res)=>{
-    res.render('home.ejs');
+
+// ROUTES
+app.get('/', async (req, res)=>{
+    // res.render('home.ejs');
+    const c = await Campground.find({});
+    res.send(c);
 })
 
 app.listen(3000, ()=>{
