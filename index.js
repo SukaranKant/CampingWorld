e028569870edddb79473c279ca77dd168aa56f76
sukaran <sukarankant765@gmail.com>
2022-08-19 23:46:01 +0530

    add reviews model with relationships with campground model

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 
@@ -62,7 +63,8 @@ app.get('/campgrounds/new', (req, res) =>{
 
 app.get('/campgrounds/:id', catchAsync(async (req, res)=>{
     const {id} = req.params;
-    const campground = await Campground.findById(id);
+    const campground = await Campground.findById(id).populate('reviews');
+    console.log(campground);
     res.render('campgrounds/show.ejs', {campground});
 }))
 
@@ -70,6 +72,22 @@ app.get('/campgrounds/:id/edit',catchAsync (async (req, res)=>{
     const {id} = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/edit.ejs', {campground});
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) =>{
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save()
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync( async (req, res, next)=>{
+    const {id, reviewId} = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull : {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`)
 }))
 
 app.all('*', (req, res, next)=>{
--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
+const Review = require('./review')
 
 const campgroundSchema = new mongoose.Schema({
     title : String,
     price : Number,
     image: String,
     description : String,
-    location : String
+    location : String,
+    reviews : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'Review'
+        }
+    ]
 });
+
+campgroundSchema.post('findOneAndDelete', async function(doc) {
+    if(doc) {
+        await Review.deleteMany({_id : {$in : doc.reviews}})
+    }
+})
 
 module.exports = mongoose.model('Campground', campgroundSchema);
